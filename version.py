3fd680a581222162a212431f56c5a2fa3a68eb1e
heanuea <g00318763@gmit.ie>
2018-09-12 22:49:24 +0100

    Added script to read python version

---
+++ version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# The shebang line(above) in any script determines the script's 
+# ability to be executed like a standalone executable 
+# without typing python beforehand in the terminal or
+#  when double clicking it in a file manager 
+# (when configured properly). 
+
+import platform 
+
+print ('This is Python version {}'.format(platform.python_version()))
