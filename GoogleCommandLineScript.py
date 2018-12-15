338d28b7b0a149465e10cb9ed1533292c2c0f9f4
heanuea <g00318763@gmit.ie>
2018-12-15 14:22:38 +0000

    fixed results error

--- GoogleCommandLineScript.py
+++ GoogleCommandLineScript.py
@@ -4,16 +4,16 @@ import json
 # Google returns JSON
 url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
 
-query = raw_input("What u want to LookFOr Matey ????? ")
+query = raw_input("What u want to Look FOr Matey ????? ")
 query = urllib.urlencode({'q': query})
 
+#Create the response object by loading the the URL response, including the query
+#e asked for above. 
 response = urllib2.urlopen(url + query).read()
-
+# Process the JSON string.
 data = json.loads(response)
 
 results = data ['responseData']['results']
-
-for results in results
 
 for result in results:
     title = resul['title']
